8b093e599f93e7128fb3cc8501e3611f5476d97f
Your Name <you@example.com>
2020-04-27 13:42:50 +0800

    Fixed ButtonClickCallback.h.

---
+++ IRExtanderTableView/Utility/ButtonClickCallback.h
@@ -0,0 +1,12 @@
+//
+//  ButtonClickCallback.h
+//  demo
+//
+//  Created by Phil on 2019/5/6.
+//  Copyright © 2019 demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef void(^ButtonClick)(UIButton *button);
+typedef void (^TextFieldCallback)(UITextField* textField);
